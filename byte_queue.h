207ba5c291339b925bc095d90ba95efd3f1dc9d5
Aladdin-Wang <17630350805@163.com>
2023-05-30 21:53:07 +0800

    增加循环覆盖的功能

--- byte_queue.h
+++ byte_queue.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-*  Copyright 2022 KK                                                        *
+*  Copyright 2022 KK (weixin:Aladdin_KK)                                    *
 *                                                                           *
 *  Licensed under the Apache License, Version 2.0 (the "License");          *
 *  you may not use this file except in compliance with the License.         *
